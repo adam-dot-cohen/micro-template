b9cf99775833b867cbbed8c9ef55fe866c161c9d
Ed Swangren <edward.swangren@epicsciences.com>
2020-01-30 10:45:38 -0800

    [ch3666] add DI to azure function.

--- src/services/Partner/Partner.Api.Functions/DataExport/DataExport.cs
+++ src/services/Partner/Partner.Api.Functions/DataExport/DataExport.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Partner.Api.Functions
+namespace Partner.Api.Functions.DataExport
 {
     public static class DataExport
     {
---
+++ src/services/Partner/Partner.Api.Functions/DataExport/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Partner.Data.Quarterspot;
+using Partner.Services.DataExport;
+
+[assembly: FunctionsStartup(typeof(Partner.Api.Functions.DataExport.Startup))]
+
+namespace Partner.Api.Functions.DataExport
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+            builder.Services.AddLogging();
+
+            builder.Services.AddTransient<IDataExporter, QsRepositoryDataExporter>();
+            builder.Services.AddTransient<IQuarterspotRepository, QuarterspotRepository>();
+        }
+    }
+}
