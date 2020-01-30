6887b1699781c548d514c262ca71777a8d91f9d7
Ed Swangren <edward.swangren@epicsciences.com>
2020-01-30 08:09:54 -0800

    [ch3666] don't commit code that doesn't compile...

--- src/services/Partner/Partner.Services/DataExport/QsRepositoryDataExporter.cs
+++ src/services/Partner/Partner.Services/DataExport/QsRepositoryDataExporter.cs
@@ -47,24 +47,25 @@ namespace Partner.Services.DataExport
             throw new NotImplementedException();
         }
 
-        public async Task ExportFirmographicsAsync()
-        {            
-            var asOfDate = DateTime.UtcNow;
+        public /*async*/ Task ExportFirmographicsAsync(params string[] x)
+        {
+            throw new NotImplementedException();
+            //var asOfDate = DateTime.UtcNow;
 
-            var transform = result.Select(r => new Firmographic
-            {
-                Customer = null,
-                Business = new LasoBusiness { Id = r.Id.ToString() },
-                EffectiveDate = asOfDate,
-                DateStarted = r.Established,
-                IndustryNaics = r.IndustryNaicsCode.ToString(),
-                IndustrySic = r.IndustrySicCode.ToString(),
-                BusinessType = r.BusinessEntityType != null ? BusinessEntityType.FromValue(r.BusinessEntityType.Value).DisplayName : null,
-                LegalBusinessName = r.LegalName,
-                BusinessPhone = NormalizationMethod.Phone10(r.Phone),
-                BusinessEin = NormalizationMethod.TaxId(r.TaxId),
-                PostalCode = NormalizationMethod.Zip5(r.Zip)
-            });
+            //var transform = result.Select(r => new Firmographic
+            //{
+            //    Customer = null,
+            //    Business = new LasoBusiness { Id = r.Id.ToString() },
+            //    EffectiveDate = asOfDate,
+            //    DateStarted = r.Established,
+            //    IndustryNaics = r.IndustryNaicsCode.ToString(),
+            //    IndustrySic = r.IndustrySicCode.ToString(),
+            //    BusinessType = r.BusinessEntityType != null ? BusinessEntityType.FromValue(r.BusinessEntityType.Value).DisplayName : null,
+            //    LegalBusinessName = r.LegalName,
+            //    BusinessPhone = NormalizationMethod.Phone10(r.Phone),
+            //    BusinessEin = NormalizationMethod.TaxId(r.TaxId),
+            //    PostalCode = NormalizationMethod.Zip5(r.Zip)
+            //});
         }
 
         public Task ExportLoanApplicationsAsync(params string[] loanIds)
