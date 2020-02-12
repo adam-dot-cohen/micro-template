dbab6841f2ed8402fb3f6210f0231b68c15e7727
jkeane <j.keane@quarterspot.com>
2020-02-12 16:05:58 -0600

    update to resource names to support single region environments

--- deploy/azure/terraform/modules/common/resourceNames/module.naming.tf
+++ deploy/azure/terraform/modules/common/resourceNames/module.naming.tf
@@ -15,32 +15,36 @@ variable "RegionMap" {
 	}
 }
 
+locals {
+	isRegional = var.environment == "prod" || var.environment == "prev" ? true : false
+}
+
 output "resourceGroup" {
-	value = "rg-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}%{ if var.role != "" }-${var.role}%{ endif }"
+	value = "rg-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }%{ if var.role != "" }-${var.role}%{ endif }"
 }
 output "storageAccount" {
-	value = "${var.tenant}${var.environment}${var.RegionMap[var.region].abbrev}%{ if var.role != "" }${var.role}%{ endif }"
+	value = "${var.tenant}${var.environment}%{ if local.isRegional }${var.RegionMap[var.region].abbrev}%{ endif }%{ if var.role != "" }${var.role}%{ endif }"
 }
 output "virtualNetwork" {
-	value= "vnet-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}"
+	value= "vnet-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }"
 }
 output "firewall" {
-	value= "fw-vnet-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}"
+	value= "fw-vnet-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }"
 }
 output "routeTable" {
-	value= "rt-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}"
+	value= "rt-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }"
 }
 output "virtualNetworkGateway" {
-	value= "vng-vnet-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}"
+	value= "vng-vnet-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }"
 }
 output "localNetworkGateway" {
-	value= "lng-vnet-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}%{ if var.role != "" }-${var.role}%{ endif }"
+	value= "lng-vnet-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }%{ if var.role != "" }-${var.role}%{ endif }"
 }
 output "virtualNetworkGatewayConnection" {
-	value= "conn-vnet-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}%{ if var.role != "" }-${var.role}%{ endif }"
+	value= "conn-vnet-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }%{ if var.role != "" }-${var.role}%{ endif }"
 }
 output "keyVault" {
-	value= "kv-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}%{ if var.role != "" }-${var.role}%{ endif }"
+	value= "kv-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }%{ if var.role != "" }-${var.role}%{ endif }"
 }
 output "networkSecurityGroup" {
 	value= "nsg-${var.tenant}-${var.environment}%{ if var.role != "" }-${var.role}%{ endif }"
@@ -49,13 +53,13 @@ output "availabilitySet" {
 	value= "as-${var.tenant}-${var.environment}-${var.role}"
 }
 output "sqlServer" {
-	value= "kv-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}%{ if var.role != "" }-${var.role}%{ endif }"
+	value= "kv-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }%{ if var.role != "" }-${var.role}%{ endif }"
 }
 output "sqlDatabase" {
 	value= "${var.tenant}-${var.environment}-${var.role}"
 }
 output "applicationGateway" {
-	value= "agw-${var.tenant}-${var.environment}-${var.RegionMap[var.region].abbrev}%{ if var.role != "" }-${var.role}%{ endif }"
+	value= "agw-${var.tenant}-${var.environment}%{ if local.isRegional }-${var.RegionMap[var.region].abbrev}%{ endif }%{ if var.role != "" }-${var.role}%{ endif }"
 }
 
 output "regions" {
