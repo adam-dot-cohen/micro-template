68916cdfeb27e66f13c09a42921be6b8dd227f19
jkeane <j.keane@quarterspot.com>
2020-02-18 12:46:30 -0600

    massage command line args for ingest

--- src/dataPipeline/Ingest.Processor/__main__.py
+++ src/dataPipeline/Ingest.Processor/__main__.py
@@ -1,36 +1,45 @@
 import sys, getopt
-from AcceptProcessor import AcceptProcessor
+from IngestProcessor import IngestProcessor
 
 def main(argv):
     orchestrationId = None
-    documentURI = None
+    manifestURI = None
+    operations = []
     try:
-        opts, args = getopt.getopt(argv, "ho:d:",["orchid=","docuri="] )
+        opts, args = getopt.getopt(argv, "ho:m:di",["orchid=","manuri="] )
     except getopt.GetoptError:
-        print ('AcceptProcessor.py -o <orchestrationId> -d <documentURI>')
+        print ('IngestProcessor.py -o <orchestrationId> -m <manifestURI> -d -i')
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print ('AcceptProcessor.py -o <orchestrationId> -d <documentURI>')
+            print ('IngestProcessor.py -o <orchestrationId> -d <manifestURI> -d -i')
             sys.exit()
         elif opt in ('-o', '--orchid'):
             orchestrationId = arg
-        elif opt in ('-d', '--docuri'):
-            documentURI = arg
+        elif opt in ('-m', '--manuri'):
+            manifestURI = arg
+        elif opt in ('-d'):
+            operations.append('diagnostics')
+        elif opt in ('-i'):
+            operations.append('ingest')
+
 
     success = True
     if orchestrationId is None: 
         print('orchestrationId is required')
         success = False
     if documentURI is None:
-        print('documentURI is required')
+        print('manifestURI is required')
+        success = False
+    if operations.count == 0:
+        print('at least one operation is required (-d for diagnostics -i for ingest)')
         success = False
 
     if not success:
         sys.exit(3)
 
-    processor = AcceptProcessor(OrchestrationId=orchestrationId, DocumentURI=documentURI)
+    processor = IngestProcessor(OrchestrationId=orchestrationId, ManifestURI=manifestURI, Operations=operations)
     processor.Exec()
 
 if __name__ == "__main__":
