d3463ed31dc0e8dbf375145eab02ed1043aa0f0d
Ed Swangren <edwardswangren@gmail.com>
2020-02-20 13:06:01 -0600

    [ch3666] add PGP encryption

---
+++ src/services/DataImport/DataImport.Core/Encryption/IPgpEncryption.cs
@@ -0,0 +1,11 @@
+﻿using Laso.DataImport.Core.IO;
+
+namespace Laso.DataImport.Core.Encryption
+{
+    public interface IPgpEncryption
+    {
+        string GenerateKey(string passPhrase);
+        void Encrypt(StreamStack streamStack, byte[] publicKey);
+        void Decrypt(StreamStack streamStack, byte[] privateKey, string passPhrase);
+    }
+}
