48ed3a2748861873220676959eef7027d1195ae7
Brandon Barry <brandon@quarterspot.com>
2020-02-24 15:06:22 -0600

    [ch3704] added partner created event

---
+++ src/services/Identity/Identity.Domain/Events/PartnerCreatedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Laso.Identity.Domain.Events
+{
+    public class PartnerCreatedEvent
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string NormalizedName { get; set; }
+    }
+}
---
+++ src/services/Identity/Identity.Infrastructure/Eventing/AzureServiceBusEventPublisher.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using Laso.Identity.Core.Messaging;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace Laso.Identity.Infrastructure.Eventing
+{
+    public class AzureServiceBusEventPublisher : IEventPublisher
+    {
+        private readonly string _connectionString;
+
+        internal AzureServiceBusEventPublisher(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task Publish(object @event)
+        {
+            var type = @event.GetType();
+
+            var managementClient = new ManagementClient(_connectionString);
+
+            var topicName = type.Name.ToLower();
+
+            var topic = await managementClient.TopicExistsAsync(topicName)
+                ? await managementClient.GetTopicAsync(topicName)
+                : await managementClient.CreateTopicAsync(topicName);
+
+            var client = new TopicClient(_connectionString, topic.Path, RetryPolicy.Default);
+
+            var bytes = JsonSerializer.SerializeToUtf8Bytes(@event);
+
+            await client.SendAsync(new Message(bytes));
+        }
+    }
+}
