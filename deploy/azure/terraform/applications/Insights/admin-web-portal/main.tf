abaee6f58fbd010c6cfdd05d875c00ecf9942052
Jason Cavaliere <jason@quarterspot.com>
2020-02-27 12:07:03 -0600

    update admin portal TF files to new format

--- deploy/azure/terraform/applications/Insights/admin-web-portal/main.tf
+++ deploy/azure/terraform/applications/Insights/admin-web-portal/main.tf
@@ -1,29 +1,31 @@
 provider "azurerm" {
   features {}
+  version = "=2.0.0"
 }
-
-
-variable "subscription_Id" {
-  type			= string
-  description = "The Azure subscriptionId for resources."
+variable "environment" {
+    type = string
 }
-variable "tenant_Id" {
-  type        	= string
-  description 	= "The Azure tenant ID for resources."
+variable "region" {
+    type = string
 }
-variable "script_principal_Id" {
-  type        	= string
-  description = "The Azure clientID (AD App registration) used for manual terraform provisioning."
-}
-variable "script_principal_secret" {
-  type        = string
-  description = "The Azure client sercret (AD App registration) used for manual terraform provisioning."
+variable "tenant" {
+    type = string
 }
 variable "buildNumber" {
-  type        = string
-  description = "build number passed from pipelines - used for docker tag"
+    type = string
+}
+variable "role" {
+    type = string
+    default = "insights"
 }
 
+locals{
+  tier = "Basic"
+  size = "B1"
+  kind = "Linux"
+  alwaysOn    = "true"
+  buildNumber = var.buildNumber
+}
 
 
 terraform {
@@ -33,77 +35,67 @@ terraform {
     }
 }
 
-locals{
-	subscriptionName 	= "LASO Development"
-	tenantId			= var.tenant_Id
-	region 				= "usc"
-	regionName 			= "South Central US"
-	location 			= "southcentralus"
-	tenant 				= "sh"
-	tenantName 			= "Shared"
-	env 				= "dev"
-	envName 			= "Dev"
-	role 				= "insights"
-	roleName 			= "Insights"
 
+##############
+# LOOKUP
+##############
+module "resourceNames" {
+  source = "../../../modules/common/resourceNames"
+
+  tenant      = var.tenant
+  region      = var.region
+  environment = var.environment
+  role        = var.role
 }
 
 
-locals{
-	appserviceplanName = "sp-${local.tenant}-${local.env}-${local.region}-laso-adminWeb"
-	appserviceName = "as-${local.tenant}-${local.env}-${local.region}-laso-adminWeb"	
-}
+
 
 #Common resource Group - created in environment provisioning
-data "azurerm_resource_group" "insightsRg" {
-  name = "rg-${local.tenant}-${local.env}-${local.region}-${local.role}"
+data "azurerm_resource_group" "rg" {
+  name = module.resourceNames.resourceGroup
 }
-data "azurerm_resource_group" "terraformGroup" {
-  name = "rg-inf-terraform"
-}
+
 
 data "azurerm_container_registry" "acr" {
-  name                     = "lasocontainers"
-  resource_group_name 		= data.azurerm_resource_group.terraformGroup.name
+  name                     = module.resourceNames.containerRegistry
+  resource_group_name 		= data.azurerm_resource_group.rg.name
 }
-
 
 
 resource "azurerm_app_service_plan" "adminAppServicePlan" {
-  name                = local.appserviceplanName
-  location            = local.location
-  resource_group_name = data.azurerm_resource_group.insightsRg.name
-  kind = "Linux"
+  name                = "${module.resourceNames.applicationServicePlan}-adminweb"
+  location            = module.resourceNames.regions[var.region].cloudRegion
+  resource_group_name = data.azurerm_resource_group.rg.name
+  kind = local.kind
   reserved = true
   sku {
-    tier = "Basic"
-    size = "B1"
+    tier = local.tier
+    size = local.size
   } 
-  
-  
 }
 
 resource "azurerm_app_service" "adminAppService" {
-  name                = local.appserviceName
-  location            = local.location
-  resource_group_name = data.azurerm_resource_group.insightsRg.name
+  name                = "${module.resourceNames.applicationService}-adminweb"
+  location            = module.resourceNames.regions[var.region].cloudRegion
+  resource_group_name = data.azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.adminAppServicePlan.id
 
   # Do not attach Storage by default
   app_settings = {
-	DOCKER_REGISTRY_SERVER_URL                = "https://${data.azurerm_container_registry.acr.login_server}"
-	DOCKER_REGISTRY_SERVER_USERNAME           = "${data.azurerm_container_registry.acr.admin_username}"
-	DOCKER_REGISTRY_SERVER_PASSWORD           = "${data.azurerm_container_registry.acr.admin_password}"
-	WEBSITES_ENABLE_APP_SERVICE_STORAGE       = false
-    DOCKER_ENABLE_CI						  = true
+  DOCKER_REGISTRY_SERVER_URL                = "https://${data.azurerm_container_registry.acr.login_server}"
+  DOCKER_REGISTRY_SERVER_USERNAME           = "${data.azurerm_container_registry.acr.admin_username}"
+  DOCKER_REGISTRY_SERVER_PASSWORD           = "${data.azurerm_container_registry.acr.admin_password}"
+  WEBSITES_ENABLE_APP_SERVICE_STORAGE       = false
+  DOCKER_ENABLE_CI						  = true
+	"Laso__CustomValue"						  = "OverriddenValue"
   }
 
   # Configure Docker Image to load on start
   site_config {
-    linux_fx_version = "DOCKER|lasocontainers.azurecr.io/laso-adminportal-web:${var.buildNumber}"
-    always_on        = "true"
+    linux_fx_version = "DOCKER|${data.azurerm_container_registry.acr.name}.azurecr.io/laso-adminportal-web:${var.buildNumber}"
+    always_on        = local.alwaysOn
   }
-
   identity {
     type = "SystemAssigned"
   }
