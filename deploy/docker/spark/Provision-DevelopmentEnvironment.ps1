835a74d4a329a5f1f251ae946a5bf1f9b672bbba
jkeane <j.keane@quarterspot.com>
2020-03-03 11:54:47 -0600

    script testing

--- deploy/docker/spark/Provision-DevelopmentEnvironment.ps1
+++ deploy/docker/spark/Provision-DevelopmentEnvironment.ps1
@@ -1,16 +1,13 @@
 [CmdletBinding(SupportsShouldProcess=$true)]
-param (
+Param (
     # Version of Spark to install locally, defaults to 2.4.5
-    [Parameter()]
     [string]$SparkVersion='2.4.5',
 
     # Version of Hadoop to install locally, defaults to 3.2.1
-    [Parameter()]
     [string]$HadoopVersion='3.2.1',
 
     # Base install directory, defaults to c:\Apache
-    [Parameter(AttributeValues)]
-    [string]$BaseInstallDirectory = 'c:\\Apache'
+    [string]$BaseInstallDirectory = 'd:\\Apache'
 )
 
 function CheckDirectories()
@@ -22,18 +19,20 @@ function CheckDirectories()
         exit 1
     }
     
-    if (-not (Test-Path -PathType Directory $BaseInstallDirectory))
+    if (-not (Test-Path -PathType Container $BaseInstallDirectory))
     {
         Write-Host "Creating base install directory"
-        New-Item -ItemType Directory $BaseInstallDirectory
+        New-Item -ItemType Directory $BaseInstallDirectory | Out-Null
     }
     
     $DownloadDirectory = "$BaseInstallDirectory\\install"
-    if (-not (Test-Path -PathType Directory $DownloadDirectory))
+    if (-not (Test-Path -PathType Container $DownloadDirectory))
     {
         Write-Host "Creating temp directory for downloads"
-        New-Item -ItemType Directory $DownloadDirectory
+        New-Item -ItemType Directory $DownloadDirectory | Out-Null
     }    
+
+    return $DownloadDirectory
 }
 
 function Add-Path([string]$PathValue, [System.EnvironmentVariableTarget]$Target)
@@ -64,6 +63,42 @@ function Set-EnvironmentVariable([string]$Name, [string]$Value, [System.Environm
     New-Item -Path env:\ -Name $Name -Value $Value
 }
 
+function DeGZip-File
+{
+    Param(
+        [string]$infile,
+        [string]$outfile = ($infile -replace '\.gz$','')
+        )
+
+    $input = New-Object System.IO.FileStream $inFile, ([IO.FileMode]::Open), ([IO.FileAccess]::Read), ([IO.FileShare]::Read)
+    $output = New-Object System.IO.FileStream $outFile, ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
+    $gzipStream = New-Object System.IO.Compression.GzipStream $input, ([IO.Compression.CompressionMode]::Decompress)
+
+    $bufsize = 1024*16
+    Write-Host "Unzipping $infile to $outfile"
+    try {
+        $buffer = New-Object byte[]($bufsize)
+        while($true)
+        {
+            $read = $gzipstream.Read($buffer, 0, $bufsize)
+    
+            if ($read -le 0) { break }
+    
+            $output.Write($buffer, 0, $read)
+            $output.Flush($true)
+        }
+    }
+    finally {
+        $gzipStream.Close()
+        $output.Close()
+        $input.Close()
+    }
+
+    Write-Host "`tUnzip complete."
+
+
+}
+
 function InstallSpark([string]$DownloadDirectory, [string]$InstallDirectory, [string]$SPARK_VERSION)
 {
     if (-not (Get-Command Expand-7Zip -ErrorAction Ignore)) {
@@ -73,17 +108,18 @@ function InstallSpark([string]$DownloadDirectory, [string]$InstallDirectory, [st
     $SPARK_PACKAGE = "spark-${SPARK_VERSION}-bin-without-hadoop"
 
     $Install_Destination = Join-Path $InstallDirectory $SPARK_PACKAGE
-    if (Test-Path -PathType Directory $Install_Destination)
+    if (Test-Path -PathType Container $Install_Destination)
     {
         Write-Verbose "Spark is already present at $Install_Destination, download skipped."        
     }
     else 
     {
-        $Package_File = Join-Path $DownloadDirectory "${SPARK_PACKAGE}.tgz"
+        $Package_File = Join-Path $DownloadDirectory "${SPARK_PACKAGE}.tar.gz"
         # Download
-        Invoke-WebRequest -UseBasicParsing -Uri "https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/${SPARK_PACKAGE}.tgz" -OutFile $Package_File        
+        Invoke-WebRequest -UseBasicParsing -Uri "https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/${SPARK_PACKAGE}.tar.gz" -OutFile $Package_File        
         # Unzip
-        Expand-Archive $Package_File -DestinationPath $DownloadDirectory
+        DeGZip-File $Package_File
+        #Expand-7Zip $Package_File -TargetPath $DownloadDirectory
 
         $Tar_File = Join-Path $DownloadDirectory "${SPARK_PACKAGE}.tar"  
         # Untar to destination
@@ -108,17 +144,18 @@ function InstallHadoop([string]$DownloadDirectory, [string]$InstallDirectory, [s
     $HADOOP_PACKAGE = "hadoop-${HADOOP_VERSION}"
 
     $Install_Destination = Join-Path $InstallDirectory $HADOOP_PACKAGE
-    if (Test-Path -PathType Directory $Install_Destination)
+    if (Test-Path -PathType Container $Install_Destination)
     {
         Write-Verbose "Hadoop is already present at $Install_Destination, download skipped."        
     }
     else 
     {
-        $Package_File = Join-Path $DownloadDirectory "${HADOOP_PACKAGE}.tgz"
+        $Package_File = Join-Path $DownloadDirectory "${HADOOP_PACKAGE}.tar.gz"
         # Download
         Invoke-WebRequest -UseBasicParsing -Uri "http://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/${HADOOP_PACKAGE}.tar.gz" -OutFile $Package_File        
         # Unzip
-        Expand-Archive $Package_File -DestinationPath $DownloadDirectory
+        DeGZip-File $Package_File
+        #Expand-7Zip $Package_File -TargetPath $DownloadDirectory
 
         $Tar_File = Join-Path $DownloadDirectory "${HADOOP_PACKAGE}.tar"  
         # Untar to destination
@@ -145,7 +182,7 @@ function InstallHadoop([string]$DownloadDirectory, [string]$InstallDirectory, [s
     return $Install_Destination
 }
 
-$DownloadDirectory = Join-Path $BaseInstallDirectory install
+$DownloadDirectory = CheckDirectories
 
 InstallHadoop $DownloadDirectory $BaseInstallDirectory $HadoopVersion
 InstallSpark $DownloadDirectory $BaseInstallDirectory $SparkVersion
