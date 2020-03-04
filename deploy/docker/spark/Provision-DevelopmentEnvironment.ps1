7129d2cba378142bb73eb46a2ff4ca54788865b1
jkeane <jkeane@keane.ms>
2020-03-04 17:00:30 -0600

    refactored logic, added package verify and check for existance

--- deploy/docker/spark/Provision-DevelopmentEnvironment.ps1
+++ deploy/docker/spark/Provision-DevelopmentEnvironment.ps1
@@ -1,3 +1,5 @@
+#Requires -RunAsAdministrator
+
 [CmdletBinding(SupportsShouldProcess=$true)]
 Param (
     # Version of Spark to install locally, defaults to 2.4.5
@@ -74,19 +76,25 @@ function DeGZip-File
     $output = New-Object System.IO.FileStream $outFile, ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
     $gzipStream = New-Object System.IO.Compression.GzipStream $input, ([IO.Compression.CompressionMode]::Decompress)
 
-    $bufsize = 1024*16
+    #$bufsize = 1024*16
     Write-Host "Unzipping $infile to $outfile"
+    #$bytesRead = 0
+    #$fileSize = (Get-Item $inFile).length
+
     try {
-        $buffer = New-Object byte[]($bufsize)
-        while($true)
-        {
-            $read = $gzipstream.Read($buffer, 0, $bufsize)
+        $gzipStream.CopyTo($output)
+        # $buffer = New-Object byte[]($bufsize)
+        # while($true)
+        # {
+        #     $read = $gzipstream.Read($buffer, 0, $bufsize)
     
-            if ($read -le 0) { break }
-    
-            $output.Write($buffer, 0, $read)
-            $output.Flush($true)
-        }
+        #     if ($read -le 0) { break }
+        #     $bytesRead += $read
+
+        #     $output.Write($buffer, 0, $read)
+        #     $output.Flush($true)
+        #     Write-Progress -Activity "Expanding GZip Archive" -status "Bytes Read $bytesRead" -percentComplete ($bytesRead / $fileSize *100)            
+        # }
     }
     finally {
         $gzipStream.Close()
@@ -99,6 +107,42 @@ function DeGZip-File
 
 }
 
+function Verify-Hash([string]$PackageFile, [string]$HashFile)
+{
+    if (-not (get-content $HashFile) -match "^.* = ([0-9abcdef]+)")
+    {
+        throw "Failed to parse hash file $HashFile"
+    }
+
+    $publishedHash = $Matches.0
+    $packageHash = Get-FileHash $PackageFIle -Algorithm SHA512
+    return $packageHash.Hash -eq $publishedHash
+
+}
+
+function Download-Package([string]$SourceURI, [string]$HashURI, [string]$DestFile)
+{
+    # force a download of the hash file everytime
+    $Hash_File = $DestFile + ".sha512"
+    Invoke-WebRequest -UseBasicParsing -Uri $HashURI -OutFile $Hash_File -ErrorAction Stop
+
+    $downloadFile = $true
+    # Check if file already exists (good for really big package downloads)
+    if (Test-Path -PathType Leaf $DestFile)
+    {
+        # package exists, check hash            
+        $downloadFile = -not (Verify-Hash $DestFile $Hash_File) # if we fail hashcheck, download again
+    }
+
+    if ($downloadFile)
+    {
+        Invoke-WebRequest -UseBasicParsing -Uri $SourceURI -OutFile $DestFile -ErrorAction Stop
+        if (-not (Verify-Hash $DestFile $Hash_File))
+        {
+            throw "Package $DestFile failed hashcheck"
+        }
+    }
+}
 function InstallSpark([string]$DownloadDirectory, [string]$InstallDirectory, [string]$SPARK_VERSION)
 {
     if (-not (Get-Command Expand-7Zip -ErrorAction Ignore)) {
@@ -115,25 +159,32 @@ function InstallSpark([string]$DownloadDirectory, [string]$InstallDirectory, [st
     else 
     {
         $Package_File = Join-Path $DownloadDirectory "${SPARK_PACKAGE}.tar.gz"
+
         # Download
-        Invoke-WebRequest -UseBasicParsing -Uri "https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/${SPARK_PACKAGE}.tar.gz" -OutFile $Package_File        
+        Download-Package    "https://ftp.wayne.edu/apache/spark/spark-${SPARK_VERSION}/${SPARK_PACKAGE}.tar.gz" `
+                            "https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/${SPARK_PACKAGE}.tar.gz.sha512" `
+                            $Package_File
+#        Download-Package "https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/${SPARK_PACKAGE}.tar.gz" $Package_File
+
         # Unzip
         DeGZip-File $Package_File
         #Expand-7Zip $Package_File -TargetPath $DownloadDirectory
 
         $Tar_File = Join-Path $DownloadDirectory "${SPARK_PACKAGE}.tar"  
         # Untar to destination
-        Expand-7Zip $Tar_File $Install_Destination
+        Expand-7Zip $Tar_File $InstallDirectory
+        Write-Host "UnTar Complete."
 
     }
 
     # Ensure environment is setup
         # Set Environment Variables
-    [System.Environment]::SetEnvironmentVariable('SPARK_HOME', $Install_Destination, [System.EnvironmentVariableTarget]::Machine)
-    Add-Path (Join-Path $Install_Destination "bin") [System.EnvironmentVariableTarget]::Machine
+    Set-EnvironmentVariable  'SPARK_HOME'  $Install_Destination  ([System.EnvironmentVariableTarget]::Machine)
+    Add-Path (Join-Path $Install_Destination "bin") ([System.EnvironmentVariableTarget]::Machine)
 
     return $Install_Destination
 }
+
 
 function InstallHadoop([string]$DownloadDirectory, [string]$InstallDirectory, [string]$HADOOP_VERSION)
 {
@@ -152,7 +203,12 @@ function InstallHadoop([string]$DownloadDirectory, [string]$InstallDirectory, [s
     {
         $Package_File = Join-Path $DownloadDirectory "${HADOOP_PACKAGE}.tar.gz"
         # Download
-        Invoke-WebRequest -UseBasicParsing -Uri "http://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/${HADOOP_PACKAGE}.tar.gz" -OutFile $Package_File        
+        # "http://ftp.wayne.edu/apache/hadoop/common/hadoop-${HADOOP_VERSION}/${HADOOP_PACKAGE}.tar.gz" `
+        Download-Package    "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=hadoop/common/hadoop-${HADOOP_VERSION}/${HADOOP_PACKAGE}.tar.gz" `
+                            "http://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/${HADOOP_PACKAGE}.tar.gz.sha512" `
+                            $Package_File 
+#        Download-Package "http://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/${HADOOP_PACKAGE}.tar.gz" $Package_File 
+
         # Unzip
         DeGZip-File $Package_File
         #Expand-7Zip $Package_File -TargetPath $DownloadDirectory
@@ -160,7 +216,7 @@ function InstallHadoop([string]$DownloadDirectory, [string]$InstallDirectory, [s
         $Tar_File = Join-Path $DownloadDirectory "${HADOOP_PACKAGE}.tar"  
         # Untar to destination
         Expand-7Zip $Tar_File $InstallDirectory
-
+        Write-Host "UnTar Complete."
     }
 
     if (-not (Test-Path (Join-Path $Install_Destination "bin\winutils.exe")))
@@ -175,14 +231,16 @@ function InstallHadoop([string]$DownloadDirectory, [string]$InstallDirectory, [s
 
     # Ensure environment is setup
         # Set Environment Variables
-    Set-EnvironmentVariable  'HADOOP_HOME'  $Install_Destination  [System.EnvironmentVariableTarget]::Machine
-    Set-EnvironmentVariable  'SPARK_DIST_CLASSPATH'  ""  [System.EnvironmentVariableTarget]::Machine
-    Add-Path (Join-Path $Install_Destination "bin") [System.EnvironmentVariableTarget]::Machine
+    Set-EnvironmentVariable  'HADOOP_HOME'  $Install_Destination  ([System.EnvironmentVariableTarget]::Machine)
+    Set-EnvironmentVariable  'SPARK_DIST_CLASSPATH'  ""  ([System.EnvironmentVariableTarget]::Machine)
+    Add-Path (Join-Path $Install_Destination "bin") ([System.EnvironmentVariableTarget]::Machine)
 
     return $Install_Destination
 }
 
 $DownloadDirectory = CheckDirectories
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 InstallHadoop $DownloadDirectory $BaseInstallDirectory $HadoopVersion
 InstallSpark $DownloadDirectory $BaseInstallDirectory $SparkVersion
