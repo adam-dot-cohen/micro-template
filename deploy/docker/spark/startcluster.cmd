907ef26582b45eb2b8b588459f5fed26b016a97a
jkeane <j.keane@quarterspot.com>
2020-03-04 10:24:14 -0600

    added cmd line param for number of workers

--- deploy/docker/spark/startcluster.cmd
+++ deploy/docker/spark/startcluster.cmd
@@ -1,1 +1,10 @@
-docker-compose up --detach --scale spark-worker=1
+@echo off
+
+if [%1] == [] (
+	SET WORKERS=1
+) ELSE (
+	SET WORKERS=%1
+)
+
+echo Starting Spark Cluster with %WORKERS% workers
+docker-compose up --detach --scale spark-worker=%WORKERS%
