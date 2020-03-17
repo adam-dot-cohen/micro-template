810edb4f84e53a02287214abaa53cce607b105be
Jay McLain <jay@quarterspot.com>
2020-03-17 16:08:47 -0500

    Remove API fetch sample from Admin Portal.

--- src/web/AdminPortal/AdminPortal.Web/ClientApp/src/app/demo/demo.module.ts
+++ src/web/AdminPortal/AdminPortal.Web/ClientApp/src/app/demo/demo.module.ts
@@ -5,13 +5,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppUiModule } from '@app/shared/app-ui.module';
 
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ThemeSampleComponent } from './theme-sample/theme-sample.component';
 
 @NgModule({
   declarations: [
     CounterComponent,
-    FetchDataComponent,
     ThemeSampleComponent
   ],
   imports: [
@@ -21,7 +19,6 @@ import { ThemeSampleComponent } from './theme-sample/theme-sample.component';
     ReactiveFormsModule,
     RouterModule.forChild([
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'theme-sample', component: ThemeSampleComponent }
     ])
   ]
