ccdea66f98e13cb3d35befe34e89b31a465259e4
Gregory Long <gregory.long@gmail.com>
2020-03-25 07:39:28 -0700

    fixed eol and updates get attributes files; [ch3730]

--- src/services/Provisioning/Provisioning.sftp/azure/create-partner-mount.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-partnerTmp=/tmp/blobfuse/$1
-partnerFiles=/home/$1/files
-container=$2
-
-mkdir -p partnerTmp
-mkdir -p partnerFiles
-
--- src/services/Provisioning/Provisioning.sftp/azure/provision-mount.sh
+++ src/services/Provisioning/Provisioning.sftp/azure/provision-mount.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Paths
+hostCmdPath="/srv/sftp/mgmt"
+tmpBlobfuse="/tmp/blobfuse"
+# create directories if they don't exist
+mkdir -p "$hostCmdPath"
+mkdir -p "$tmpBlobfuse"
 
 if [[ -z $AZURE_STORAGE_ACCOUNT ]]; then
         export AZURE_STORAGE_ACCOUNT=lsitstordevue
@@ -7,4 +13,4 @@ if [[ -z $AZURE_STORAGE_ACCESS_KEY ]]; then
         export AZURE_STORAGE_ACCESS_KEY=T/Bz/6Vb5NMATQHJMKE0fE/PHKI30OixVzeIKMjWjyAwiDy2CoMK2pqPl4gRX/mzQpza/B89tzSgbBu/uF4HZg==
 fi
 
-blobfuse /srv/sftp/mgmt --tmp-path=/tmp/blobfuse -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --container-name=provisioning --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
+blobfuse "$hostCmdPath" --tmp-path="$tmpBlobfuse" -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --container-name=provisioning --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
