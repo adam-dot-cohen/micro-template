63a6591899626dd0e303707385ba2085e182dc02
jkeane <jkeane@keane.ms>
2020-03-26 12:11:07 -0500

    testing of azure function entry point

--- src/dataPipeline/data-router/__dbs-main__.py
+++ src/dataPipeline/data-router/__dbs-main__.py
@@ -6,18 +6,24 @@ from runtime.router import (RouterRuntime, RouterCommand)
 
 
 def main(argv):
-    commandValue = argv[1]
+    commandValue = argv[0]
     if not isinstance(commandValue, str):
         raise Exception(f'data-router: expecting a string argument, received {type(commandValue)}')
 
     try:
         command: RouterCommand = CommandSerializationService.Loads(commandValue, RouterCommand)
-        
-        runtime = RouterRuntime(command=command)
-        runtime.Exec()
     except Exception as e:
-        print('Exception caught during pipeline execution')
+        print('Failed to parse incoming json as RouterCommand')
         traceback.print_exc(file=sys.stdout)
+    else:
+        try:
+        
+            runtime = RouterRuntime(command=command)
+            runtime.Exec()
+
+        except Exception as e:
+            print('Exception caught during pipeline execution')
+            traceback.print_exc(file=sys.stdout)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
