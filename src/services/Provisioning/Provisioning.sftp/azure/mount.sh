dec84e26083512872180043ce981cf4a2122630c
Gregory Long <gregory.long@gmail.com>
2020-03-26 14:26:23 -0700

    hard-coding dev storage until az cli is working;[ch3730]

--- src/services/Provisioning/Provisioning.sftp/azure/mount.sh
+++ src/services/Provisioning/Provisioning.sftp/azure/mount.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#TODO: move these to the container or other location vs in files
+#TODO: install az cli and use it to access key vault to get these values
 if [[ -z $AZURE_STORAGE_ACCOUNT ]]; then
-	export AZURE_STORAGE_ACCOUNT=lsitstordevue
+	export AZURE_STORAGE_ACCOUNT=lasodevinsightsescrow
 fi
 if [[ -z $AZURE_STORAGE_ACCESS_KEY ]]; then
-	export AZURE_STORAGE_ACCESS_KEY=T/Bz/6Vb5NMATQHJMKE0fE/PHKI30OixVzeIKMjWjyAwiDy2CoMK2pqPl4gRX/mzQpza/B89tzSgbBu/uF4HZg==
+	export AZURE_STORAGE_ACCESS_KEY=eULyndJOh0OyFSTSa0ezk06cpg4GTY9IkmfPAw6lDyDlSrb7PuORvPF4/e4y/Xbda+nw2hTh9pg613cTlG2cuw==
 fi
 blobfuse $1 --tmp-path=$2 -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --container-name=$3 --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120 -o allow_other
--- src/services/Provisioning/Provisioning.sftp/azure/provision-mount.sh
+++ src/services/Provisioning/Provisioning.sftp/azure/provision-mount.sh
@@ -6,11 +6,12 @@ tmpBlobfuse="/tmp/blobfuse"
 mkdir -p "$hostCmdPath"
 mkdir -p "$tmpBlobfuse"
 
+#TODO: install az cli and use it to access key vault to get these values
 if [[ -z $AZURE_STORAGE_ACCOUNT ]]; then
-        export AZURE_STORAGE_ACCOUNT=lsitstordevue
+        export AZURE_STORAGE_ACCOUNT=lasodevinsightsescrow
 fi
 if [[ -z $AZURE_STORAGE_ACCESS_KEY ]]; then
-        export AZURE_STORAGE_ACCESS_KEY=T/Bz/6Vb5NMATQHJMKE0fE/PHKI30OixVzeIKMjWjyAwiDy2CoMK2pqPl4gRX/mzQpza/B89tzSgbBu/uF4HZg==
+        export AZURE_STORAGE_ACCESS_KEY=eULyndJOh0OyFSTSa0ezk06cpg4GTY9IkmfPAw6lDyDlSrb7PuORvPF4/e4y/Xbda+nw2hTh9pg613cTlG2cuw==
 fi
 
 blobfuse "$hostCmdPath" --tmp-path="$tmpBlobfuse" -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --container-name=provisioning --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
