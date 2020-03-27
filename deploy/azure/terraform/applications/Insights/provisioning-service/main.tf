2447c46b36ad8d41cde385bcad7f9b327ae0409e
Jason Cavaliere <jason@quarterspot.com>
2020-03-26 19:32:46 -0500

    provisioning servic to use app service moduile [ch3853]

--- deploy/azure/terraform/applications/Insights/provisioning-service/main.tf
+++ deploy/azure/terraform/applications/Insights/provisioning-service/main.tf
@@ -45,20 +45,33 @@ terraform {
     }
 }
 
+module "serviceNames" {
+  source = "../servicenames"
+}
+module "resourceNames" {
+  source = "../../../modules/common/resourceNames"
+
+  tenant      = var.tenant
+  region      = var.region
+  environment = var.environment
+  role        = var.role
+}
 
 data "azurerm_resource_group" "rg" {
   name = module.resourceNames.resourceGroup
-}
-
-data  "azurerm_storage_account" "storageAccount" {
-  name                     = module.resourceNames.storageAccount
-  resource_group_name      = data.azurerm_resource_group.rg.name
 }
 
 data "azurerm_key_vault" "kv" {
   name                     = module.resourceNames.keyVault
   resource_group_name 		= data.azurerm_resource_group.rg.name
 }
+
+
+data  "azurerm_storage_account" "storageAccount" {
+  name                     = module.resourceNames.storageAccount
+  resource_group_name      = data.azurerm_resource_group.rg.name
+}
+
 
 module "Service" {
   source = "../../../modules/common/appservice"
@@ -74,6 +87,7 @@ module "Service" {
     buildNumber = var.buildNumber
     ciEnabled=true,
     capacity=var.capacity
+    dockerRepo="laso-provisioning-api"
   }
   app_settings={
     AzureDataLake__BaseUrl=data.azurerm_storage_account.storageAccount.primary_blob_endpoint
@@ -81,45 +95,3 @@ module "Service" {
     AzureKeyVault__VaultBaseUrl = data.azurerm_key_vault.kv.vault_uri
   }
 }
-
-
-
-
-resource "azurerm_app_service" "adminAppService" {
-  name                = "${module.resourceNames.applicationService}-${module.serviceNames.provisioningService}"
-  location            = module.resourceNames.regions[var.region].cloudRegion
-  resource_group_name = data.azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.adminAppServicePlan.id
-
-  # Do not attach Storage by default
-  app_settings = {
-  DOCKER_REGISTRY_SERVER_URL                = "https://${data.azurerm_container_registry.acr.login_server}"
-  DOCKER_REGISTRY_SERVER_USERNAME           = "${data.azurerm_container_registry.acr.admin_username}"
-  DOCKER_REGISTRY_SERVER_PASSWORD           = "${data.azurerm_container_registry.acr.admin_password}"
-  WEBSITES_ENABLE_APP_SERVICE_STORAGE       = false
-  DOCKER_ENABLE_CI						  = true
-  ASPNETCORE_FORWARDEDHEADERS_ENABLED = true
-	# ASPNETCORE_ENVIRONMENT = "Development"  We don't use this becuase it throws off the client side.  
-  # we need to revisit if we want to use appsettings.{env}.config overrides though.
-
-  }
-
-  # Configure Docker Image to load on start
-  site_config {
-    linux_fx_version = "DOCKER|${data.azurerm_container_registry.acr.name}.azurecr.io/laso-provisioning-api:${local.buildNumber}"
-    always_on        = local.alwaysOn
-  }
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-
-resource "azurerm_key_vault_access_policy" "example" {
-  key_vault_id = data.azurerm_key_vault.kv.id
-  tenant_id = data.azurerm_subscription.current.tenant_id
-  object_id = azurerm_app_service.adminAppService.identity[0].principal_id
-  key_permissions = ["get","list","Create"]
-  secret_permissions = ["get","list","Set"]
-}
-
