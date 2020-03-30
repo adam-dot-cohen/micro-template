615333c9d676b4328d497f25b0a70fcf1d849abd
jkeane <j.keane@quarterspot.com>
2020-03-29 22:09:25 -0500

    updated for mount points

--- deploy/docker/python/mount-blobfuse.sh
+++ deploy/docker/python/mount-blobfuse.sh
@@ -15,16 +15,16 @@ IFS=$'\n\t'
 
 #blobfuse /mnt/escrow --use-https=true --tmp-path=/tmp/blobfuse --container-name=${AZURE_STORAGE_ACCOUNT_CONTAINER} -o allow_other
 
-set AZURE_CONFIG_DIR=/etc/blobcfg/insights
+#set AZURE_CONFIG_DIR=/etc/blobcfg/insights
 #goofys abfs://raw@lasodevinsights.dfs.core.windows.net /mnt/raw 
 
 # ./goofys wasb://raw@lasodevinsights.blob.core.windows.net /mnt/raw 
 
 #blobfuse /mnt/escrow   --use-https=true --tmp-path=/mnt/blobfusetmp/escrow -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --config-file=/etc/blobcfg/escrow-configuration.cfg --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
 
-#blobfuse /mnt/raw --use-https=true --tmp-path=/mnt/blobfusetmp/raw --config-file=/etc/blobcfg/raw-configuration.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
-#blobfuse /mnt/rejected --use-https=true --tmp-path=/mnt/blobfusetmp/rejected --config-file=/etc/blobcfg/rejected-configuration.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
-#blobfuse /mnt/curated --use-https=true --tmp-path=/mnt/blobfusetmp/curated --config-file=/etc/blobcfg/curated-configuration.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
+blobfuse /mnt/raw --use-https=true --tmp-path=/mnt/blobfusetmp/raw --config-file=/etc/blobcfg/raw-configuration.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
+blobfuse /mnt/rejected --use-https=true --tmp-path=/mnt/blobfusetmp/rejected --config-file=/etc/blobcfg/rejected-configuration.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
+blobfuse /mnt/curated --use-https=true --tmp-path=/mnt/blobfusetmp/curated --config-file=/etc/blobcfg/curated-configuration.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
 
 # run the command passed to us
 while true; do
