9a62e2deacfe300ef41c024c1648545c1c8ecc2f
jkeane <jkeane@keane.ms>
2020-03-30 11:36:30 -0500

    update build dist script to include version number from module

--- src/dataPipeline/build-dist.ps1
+++ src/dataPipeline/build-dist.ps1
@@ -1,11 +1,12 @@
 # SYNTAX:  .\build-dist.ps1 data-router data-router
 
 param (
-	$distName,
-	$rootProject
+	[string]$RootProject,
+	[string]$DistName=$RootProject,
+	[switch]$Docker
 )
 
-$distroot="dist\$distname"
+$distroot="dist\$DistName"
 
 if (-not (test-path $distroot)) {
 	new-item -itemtype directory $distroot | Out-Null
@@ -16,24 +17,33 @@ $exclude = @()
 $excludeDir = @("__pycache__", "env", "ARCHIVE", ".mypy_cache")
 $sourceFiles = "*.py"
 
-Copy-Item -Path "$($rootProject)\requirements.txt" $distroot -Verbose
-&robocopy $rootProject $distroot\$_ $sourceFiles /S /XD "__pycache__" "env" "ARCHIVE" ".mypy_cache"
+Copy-Item -Path "$($RootProject)\requirements.txt" $distroot -Verbose
+&robocopy $RootProject $distroot\$_ $sourceFiles /S /XD "__pycache__" "env" "ARCHIVE" ".mypy_cache"
 
 $libraries | % { &robocopy $_ $distroot\$_ $sourceFiles /S /XD "__pycache__" "env" "ARCHIVE" ".mypy_cache" }
 	
-python -m zipapp $distroot -o "dist\$distName-1.0.zip"
+# get version
+if (-not (Get-Content .\__init__.py) -match '^__version__\s+=\s+\"(?<version>\d+\.\d+\.\d+)\"' -or [string]::IsNullOrEmpty($Matches.version))
+{
+	Write-Host "Failed to get version number from __init__.py.  Ensure file has a property formatted version tag."
+	return
+}
+
+python -m zipapp $distroot -o "dist\$DistName-$($Matches.version).zip"
 rd $distroot -recurse
 
-copy "dist\$distName-1.0.zip" c:\docker\mnt\data\app  
+if ($Docker) {
+	copy "dist\$DistName-1.0.zip" c:\docker\mnt\data\app  
+}
 
 # Write-Host "Copy to $DISTROOT"
-# Copy-Item -Path "$distName-setup*.py" $DISTROOT -Verbose
+# Copy-Item -Path "$DistName-setup*.py" $DISTROOT -Verbose
 
 # 'framework','steplibrary' | % { & robocopy "$_" "$DISTROOT\$_" *.py /S /XD env __pycache__ ARCHIVE .mypy_cache }
 # Compress-Archive -Path $DISTROOT\* -DestinationPath "$($DISTROOT)_1.0.0.zip" -Force -Verbose
 
 
-# & python ".\$($distName)-setup.py" sdist --formats=zip
+# & python ".\$($DistName)-setup.py" sdist --formats=zip
 
-# copy-item "dist\$($distName)*.zip" "c:\docker\mnt\data\app"
+# copy-item "dist\$($DistName)*.zip" "c:\docker\mnt\data\app"
 
