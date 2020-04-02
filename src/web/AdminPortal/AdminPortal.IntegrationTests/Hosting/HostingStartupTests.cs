c9a28e9f30a185029626f860c6f27bb95e0504ec
Jay McLain <jay_mclain@msn.com>
2020-04-02 14:20:02 -0500

    Don't include KeyVault configuration for Development.

---
+++ src/web/AdminPortal/AdminPortal.IntegrationTests/Hosting/HostingStartupTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace Laso.AdminPortal.IntegrationTests.Hosting
+{
+    public abstract class HostingStartupTests : IntegrationTestBase
+    {
+        public class When_Hosted : HostingStartupTests
+        {
+            [Fact]
+            public void Services_Should_Be_Configured()
+            {
+                Services.ShouldNotBeNull();
+            }
+
+            [Fact]
+            public void Configuration_Should_Be_Configured()
+            {
+                Configuration.ShouldNotBeNull();
+                Configuration.AsEnumerable().ShouldNotBeEmpty();
+            }
+        }
+    }
+}
---
+++ src/web/AdminPortal/AdminPortal.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Laso.AdminPortal.Web;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Laso.AdminPortal.IntegrationTests
+{
+    public abstract class IntegrationTestBase
+    {
+        protected IntegrationTestBase()
+        {
+            var workingDirectory = 
+                Path.GetDirectoryName(Assembly.GetAssembly(typeof(IntegrationTestBase)).Location);
+
+            var rootDirectory = $@"{workingDirectory}\..\..\..\..\..\..";
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile($@"{rootDirectory}\web\AdminPortal\AdminPortal.Web\appsettings.json")
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var hostBuilder = Program.CreateHostBuilder(configuration, null);
+            var host = hostBuilder.Build();
+
+            Services = host.Services;
+            Configuration = host.Services.GetRequiredService<IConfiguration>();
+        }
+
+        protected IServiceProvider Services { get; }
+        protected IConfiguration Configuration { get; }
+    }
+}
