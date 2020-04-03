6a2892fe5b57d5c1e5de006f9672f1f38992149a
jkeane <jkeane@keane.ms>
2020-04-02 19:21:10 -0500

    adding spark/hadoop

--- deploy/docker/python/Image.ps1
+++ deploy/docker/python/Image.ps1
@@ -10,7 +10,7 @@ if ($Build) {
     & docker build -t local/fuse .
 }
 if ($Run) {
-    & docker run --rm  --device /dev/fuse --cap-add SYS_ADMIN --cap-add MKNOD -d --name $Name local/fuse 
+    & docker run --rm  --device /dev/fuse --cap-add SYS_ADMIN --cap-add MKNOD -d --mount type=bind,source=S:\Docker,target=/mnt/local --name $Name local/fuse 
 }
 if ($Shell) {
     & docker exec -it $Name bash
