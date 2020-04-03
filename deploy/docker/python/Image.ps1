9202fb296c253a8d1212fdcd6e67bf10bc288ce6
jkeane <jkeane@keane.ms>
2020-04-03 15:04:53 -0500

    blobfuse work

--- deploy/docker/python/Image.ps1
+++ deploy/docker/python/Image.ps1
@@ -10,7 +10,8 @@ if ($Build) {
     & docker build -t local/fuse .
 }
 if ($Run) {
-    & docker run --rm  --device /dev/fuse --cap-add SYS_ADMIN --cap-add MKNOD -d --mount type=bind,source=S:\Docker,target=/mnt/local --name $Name local/fuse 
+    & docker run --rm --cap-add SYS_ADMIN MKNOD --device /dev/fuse -d --mount type=bind,source=S:\Docker\local,target=/mnt/local --name $Name local/fuse 
+#    & docker run --rm --privileged --device /dev/fuse -d --mount type=bind,source=S:\Docker\local,target=/mnt/local --name $Name local/fuse 
 }
 if ($Shell) {
     & docker exec -it $Name bash
