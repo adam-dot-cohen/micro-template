b1a20740af5fb8eb9c72b8a6255cbf8e25cf0efb
jsoto-qs <44030038+jsoto-qs@users.noreply.github.com>
2020-04-07 18:18:30 -0500

    added cluster pool instance for job deployment [ch3966]

--- src/dataPipeline/data-router/__init__.py
+++ src/dataPipeline/data-router/__init__.py
@@ -1,1 +1,2 @@
 __version__ = "0.1.0"
+__dbrClusterPoolName__ = "Standard_DS3_v2 --MinIdle 4"
--- src/dataPipeline/deploy-dist.ps1
+++ src/dataPipeline/deploy-dist.ps1
@@ -59,20 +59,32 @@ dbfs rm -r dbfs:/$databricksDestFolder
 dbfs cp -r $appFolder dbfs:/$databricksDestFolder
 
 
-#create job
+
+###create job
+#ToDo: 1)adjust cluster/other attributes depending on the app, 2)allow array of libraries/init_scripts
+
+# get instance pool name
+$varFileName = "$destinationFolder\__init__.py"
+if (-not ((Get-Content $varFileName) | ForEach-Object {$_ -match '^__dbrClusterPoolName__\s+=\s+\"(?<dbrClusterPoolName>[a-zA-Z0-9_ -]+)\"'}) -or [string]::IsNullOrEmpty($matches.dbrClusterPoolName))
+{
+	Write-Host "Failed to get databricks instance pool name from $varFileName."
+	return
+}
+
+$job_instancePoolId = (databricks instance-pools list --output JSON | jq --arg poolName $matches.dbrClusterPoolName -c '.instance_pools[] | select( .instance_pool_name == $poolName ) ' | jq .default_tags.DatabricksInstancePoolId)
 $job_initScript = "dbfs:/$databricksDestFolder/init_scripts/install_requirements.sh"
 $job_library = "dbfs:/$databricksDestFolder/$appFileName"
 $job_pythonFile = "dbfs:/$databricksDestFolder/__dbs-main__.py"
 $jobSettingsFile = "$($destinationFolder)\dbr-job-settings.json"
 
+
 Set-Content -Path $jobSettingsFile -Force -Verbose -Value `
     (Get-Content -Path .\dbr-job-settings-tmpl.json | `
-        jq --arg jobName $DistName --arg init_script $job_initScript --arg library $job_library --arg python_file $job_pythonFile `
-            '.name=$jobName | .new_cluster.init_scripts[0].dbfs.destination=$init_script | .libraries[0].jar=$library | .spark_python_task.python_file=$python_file' 
+        jq --arg jobName $DistName --arg init_script $job_initScript --arg library $job_library --arg python_file $job_pythonFile --arg poolId $job_instancePoolId `
+            '.name=$jobName | .new_cluster.init_scripts[0].dbfs.destination=$init_script | .new_cluster.instance_pool_id=$poolId | .libraries[0].jar=$library | .spark_python_task.python_file=$python_file' 
 	)
 
 Get-Content -Path $jobSettingsFile 
-#ToDo: 1)adjust cluster/other attributes depending on the app, 2)allow array of libraries/init_scripts
 
 databricks jobs create --json-file $jobSettingsFile 
 
