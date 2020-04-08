a0a87533bab54769f04a91b58bc7f0409fb76a9d
Gregory Long <gregory.long@gmail.com>
2020-04-08 15:02:42 -0700

    updated sftp vm setup to be mostly automated and include hardening; [ch3987]

--- src/services/Provisioning/Provisioning.sftp/vm/install/mount/provision-mount.sh
+++ src/services/Provisioning/Provisioning.sftp/vm/install/mount/provision-mount.sh
@@ -11,10 +11,10 @@ mkdir -p "$tmpBlobfuse"
 
 #TODO: install az cli and use it to access key vault to get these values
 if [[ -z $AZURE_STORAGE_ACCOUNT ]]; then
-        export AZURE_STORAGE_ACCOUNT=lasodevinsightsescrow
+#EXPORT AZURE_STORAGE_ACCOUNT
 fi
 if [[ -z $AZURE_STORAGE_ACCESS_KEY ]]; then
-        export AZURE_STORAGE_ACCESS_KEY=eULyndJOh0OyFSTSa0ezk06cpg4GTY9IkmfPAw6lDyDlSrb7PuORvPF4/e4y/Xbda+nw2hTh9pg613cTlG2cuw==
+#EXPORT AZURE_STORAGE_ACCESS_KEY
 fi
 
 blobfuse "$hostCmdPath" --tmp-path="$tmpBlobfuse" -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --container-name=provisioning --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
