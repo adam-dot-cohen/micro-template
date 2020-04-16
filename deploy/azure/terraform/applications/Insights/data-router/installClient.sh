021d28eb019178d0999d543137a5c1335ab200e1
Jason Cavaliere <jason@quarterspot.com>
2020-04-16 09:01:36 -0500

    mirrir DQ packaging / deploy strategy in DR [ch3923]

--- deploy/azure/terraform/applications/Insights/data-router/installClient.sh
+++ deploy/azure/terraform/applications/Insights/data-router/installClient.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 sudo apt-get install -y python3-setuptools
+sudo apt-get install jq
 pip3 install wheel
 pip3 install databricks-cli
 sudo ln -s /home/vsts/.local/bin/databricks /usr/local/bin/databricks
