c0b48913737e1b4c0c410523593c1ac85ec7e07a
jkeane <jkeane@keane.ms>
2020-04-16 19:11:51 -0500

    tests for schema manager

--- src/dataPipeline/framework/tests/test_SchemaManager.py
+++ src/dataPipeline/framework/tests/test_SchemaManager.py
@@ -2,20 +2,24 @@ import unittest
 from framework.schema import *
 
 class Test_test_SchemaManager(unittest.TestCase):
-    def test_get_demographics_weak_cerberus(self):
-        success, schema = SchemaManager().get('demographics', SchemaType.weak, 'cerberus')        
+    def test_get_demographics_weak_cerberus_1(self):
+        success, schema = SchemaManager().get('demographic', SchemaType.weak, 'cerberus')        
+        self.assertEqual(success, True)
+
+    def test_get_demographics_weak_cerberus_2(self):
+        success, schema = SchemaManager().get('Demographic', SchemaType.weak, 'cerberus')        
         self.assertEqual(success, True)
 
     def test_get_demographics_weak_error_cerberus(self):
-        success, schema = SchemaManager().get('demographics', SchemaType.weak_error, 'cerberus')        
+        success, schema = SchemaManager().get('demographic', SchemaType.weak_error, 'cerberus')        
         self.assertEqual(success, True)
 
     def test_get_demographics_strong_cerberus(self):
-        success, schema = SchemaManager().get('demographics', SchemaType.strong, 'cerberus')        
+        success, schema = SchemaManager().get('demographic', SchemaType.strong, 'cerberus')        
         self.assertEqual(success, True)
 
     def test_get_demographics_strong_error_cerberus(self):
-        success, schema = SchemaManager().get('demographics', SchemaType.strong_error, 'cerberus')        
+        success, schema = SchemaManager().get('demographic', SchemaType.strong_error, 'cerberus')        
         self.assertEqual(success, True)
 
 if __name__ == '__main__':
