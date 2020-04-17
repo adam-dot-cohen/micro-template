cd549dfcc882810ec1bc8ee29d79eb300c30d1eb
jkeane <jkeane@keane.ms>
2020-04-16 19:29:27 -0500

    added accounttransaction schemas to class

--- src/dataPipeline/framework/schema.py
+++ src/dataPipeline/framework/schema.py
@@ -2,6 +2,9 @@ import copy
 from pyspark.sql.types import *
 from enum import Enum, auto
 from collections import OrderedDict
+from datetime import datetime
+
+to_date = (lambda myDateTime:  datetime.strptime(myDateTime, '%Y-%m-%d %H:%M:%S'))
 
 class SchemaType(Enum):
     weak = auto(),
@@ -17,6 +20,7 @@ class SchemaType(Enum):
         return self in [SchemaType.weak_error, SchemaType.strong_error]
 
 
+# TODO: Collapse down to just strong, mutate to weak on request
 class SchemaManager:
     _schemas = {
             'demographic': {
@@ -34,18 +38,43 @@ class SchemaManager:
                                     ( 'CREDIT_SCORE',         {'type': 'string'} ),
                                     ( 'CREDIT_SCORE_SOURCE',  {'type': 'string'} )
                                 ])
+                },
+            'accounttransaction': {
+                    SchemaType.strong : OrderedDict([
+                                    ('LASO_CATEGORY',           {'type': 'string'}),
+                                    ('AcctTranKey_id',          {'type': 'integer',  'coerce': int}),
+                                    ('ACCTKey_id',              {'type': 'integer',  'coerce': int}),
+                                    ('TRANSACTION_DATE',        {'type': 'datetime', 'coerce': to_date}),
+                                    ('POST_DATE',               {'type': 'datetime', 'coerce': to_date}),
+                                    ('TRANSACTION_CATEGORY',    {'type': 'string'}),
+                                    ('AMOUNT',                  {'type': 'float',    'coerce': float}),
+                                    ('MEMO_FIELD',              {'type': 'string'}),
+                                    ('MCC_CODE',                {'type': 'string'})
+                                ]),
+                    SchemaType.weak : OrderedDict([
+                                    ('LASO_CATEGORY',           {'type': 'string'}),
+                                    ('AcctTranKey_id',          {'type': 'string'}),
+                                    ('ACCTKey_id',              {'type': 'string'}),
+                                    ('TRANSACTION_DATE',        {'type': 'string'}),
+                                    ('POST_DATE',               {'type': 'string'}),
+                                    ('TRANSACTION_CATEGORY',    {'type': 'string'}),
+                                    ('AMOUNT',                  {'type': 'string'}),
+                                    ('MEMO_FIELD',              {'type': 'string'}),
+                                    ('MCC_CODE',                {'type': 'string'})
+                                ])
                 }
         }    
     _TypeMap = {
-        "string" : StringType,
-        "number" : DoubleType,
-        "float"  : FloatType,
-        "integer": IntegerType,
-        "boolean": BooleanType,
-        "object" : StructType,
-        "array"  : ArrayType
+        "string"    : StringType,
+        "number"    : DoubleType,
+        "float"     : DoubleType,
+        "integer"   : IntegerType,
+        "boolean"   : BooleanType,
+        "object"    : StructType,
+        "array"     : ArrayType,
+        'datetime'  :TimestampType 
         }
-
+    
     def get(self, name: str, schema_type: SchemaType, target: str):
         schema_dict = self._schemas.get(name.lower(), None)
         
@@ -66,6 +95,6 @@ class SchemaManager:
         if target.lower() == 'cerberus':
             return True, schema
 
-        return True, StructType([StructField(k,self._TypeMap.get(v['type'])(),True) for k,v in schema.items()])
+        return True, StructType([StructField(k, self._TypeMap.get(v['type'])(), True) for k,v in schema.items()])
 
     
