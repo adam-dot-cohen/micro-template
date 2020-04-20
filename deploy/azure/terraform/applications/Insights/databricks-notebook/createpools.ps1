69bce2babf2e031286911b4338e2da0ececaf380
Jason Cavaliere <jason@quarterspot.com>
2020-04-20 14:55:02 -0500

    create cluster pools from devops  [ch3923]

---
+++ deploy/azure/terraform/applications/Insights/databricks-notebook/createpools.ps1
@@ -0,0 +1,30 @@
+
+$env:ApplicationEnvironment = "dev"
+$poolList= get-content "./clusters/$($env:ApplicationEnvironment).json" | ConvertFrom-Json
+
+
+
+new-item temp -ItemType Directory -Force
+$existing = ((databricks instance-pools list --output JSON)  | ConvertFrom-Json).instance_pools
+function poolExists($existing,[string] $name){
+  foreach ($ePool in $existing) {
+    if($ePool.instance_pool_name -eq $name) {return $ePool}
+  }
+  return $null
+}
+
+
+foreach ($pool in $poolList) {
+    $poolName = $pool.instance_pool_name
+    Set-Content "./temp/template.json" -Value ($pool | ConvertTo-Json) -Force
+    $exists =poolExists $existing $poolName
+    if($exists)
+    {
+      write-host "$poolName found, skipping"
+    #  databricks instance-pools edit --json-file './temp/template.json'
+    }
+    else{
+      write-host "$poolName not found, creating"
+      databricks instance-pools create --json-file './temp/template.json'
+    }
+  }
--- deploy/azure/terraform/applications/Insights/databricks-notebook/runnotebook.ps1
+++ deploy/azure/terraform/applications/Insights/databricks-notebook/runnotebook.ps1
@@ -1,3 +1,8 @@
 
+#check if notebookk exists
+#if !esixts
+databricks runs submit --json-file "$($env:WORKING_DIRECTORY)/runNotebook.json" "{key:value}"
 
-databricks runs submit --json-file "$($env:WORKING_DIRECTORY)/runNotebook.json"
+
+#check if cluster exists
+#do the thing....
