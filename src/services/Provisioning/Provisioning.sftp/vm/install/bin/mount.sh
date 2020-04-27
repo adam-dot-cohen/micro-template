765f8b6d1b5f2f3903cdcd6705dbe8e0f74e1ef1
Gregory Long <gregory.long@gmail.com>
2020-04-27 13:07:30 -0700

    updated vm provisioning to utilize managed identity; [ch3993]

--- src/services/Provisioning/Provisioning.sftp/vm/install/bin/mount.sh
+++ src/services/Provisioning/Provisioning.sftp/vm/install/bin/mount.sh
@@ -2,11 +2,10 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-#TODO: install az cli and use it to access key vault to get these values
-if [[ -z $AZURE_STORAGE_ACCOUNT ]]; then
-#EXPORT AZURE_STORAGE_ACCOUNT       
+if [ -n $4 ] && [ $4 == "true" ]; then
+	allow="allow_root"
+else
+	allow="allow_other"
 fi
-if [[ -z $AZURE_STORAGE_ACCESS_KEY ]]; then
-#EXPORT AZURE_STORAGE_ACCESS_KEY 
-fi
-blobfuse $1 --tmp-path=$2 -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --container-name=$3 --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120 -o allow_other
+
+blobfuse $1 --tmp-path=$2 -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --config-file=$3 --log-level=LOG_ERR --file-cache-timeout-in-seconds=120 -o "$allow"
