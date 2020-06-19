d5e655eeeed76960fdefb87cbc250b287aec8b7a
jkeane <jkeane@keane.ms>
2020-06-19 15:54:37 -0500

    added helper to dump arbitrary class

--- src/dataPipeline/framework/util.py
+++ src/dataPipeline/framework/util.py
@@ -1,6 +1,7 @@
 import uuid
 import random
 import string
+import json
 from enum import Enum
 from dataclasses import fields as datafields
 from dataclasses import is_dataclass
@@ -75,3 +76,29 @@ def to_bool(val):
 def random_string(length=8):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
+
+import inspect
+
+class ObjectEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, "to_json"):
+            return self.default(obj.to_json())
+        elif hasattr(obj, "__dict__"):
+            d = dict(
+                (key, value)
+                for key, value in inspect.getmembers(obj)
+                if not key.startswith("__")
+                and not inspect.isabstract(value)
+                and not inspect.isbuiltin(value)
+                and not inspect.isfunction(value)
+                and not inspect.isgenerator(value)
+                and not inspect.isgeneratorfunction(value)
+                and not inspect.ismethod(value)
+                and not inspect.ismethoddescriptor(value)
+                and not inspect.isroutine(value)
+            )
+            return self.default(d)
+        return obj
+
+def dump_class(logger, prefix, cls):
+    logger(f'{prefix} {json.dumps(cls, indent=2, cls=ObjectEncoder)}')
