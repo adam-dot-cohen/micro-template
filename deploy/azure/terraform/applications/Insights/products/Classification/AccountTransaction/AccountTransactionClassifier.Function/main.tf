a08284e3f484fc92ff6cda9a81200c1d3987a53b
Jay McLain <jay_mclain@msn.com>
2020-08-13 11:46:02 -0500

    Event Grid trigger config for transaction classify.

--- deploy/azure/terraform/applications/Insights/products/Classification/AccountTransaction/AccountTransactionClassifier.Function/main.tf
+++ deploy/azure/terraform/applications/Insights/products/Classification/AccountTransaction/AccountTransactionClassifier.Function/main.tf
@@ -64,10 +64,10 @@ data "azurerm_storage_account" "storageAccountescrow" {
   name                  = "${module.resourceNames.storageAccount}escrow"
   resource_group_name   = data.azurerm_resource_group.rg.name
 }
-    
+
 module "function" {
   source = "../../../../../../modules/common/function"
-  application_environment=module.resourceNames.applicationEnvironment
+  application_environment = module.resourceNames.applicationEnvironment
 
   service_settings = {
     tshirt          = var.tShirt
@@ -82,4 +82,62 @@ module "function" {
     AzureWebJobsStorage = data.azurerm_storage_account.storageAccount.primary_connection_string
     WEBSITE_HTTPLOGGING_RETENTION_DAYS = 1
   }  
+}
+
+data "azurerm_function_app" "fn" {
+  name                  = "${module.resourceNames.function}-${module.serviceNames.transactionClassifier}"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_template_deployment" "function_keys" {
+  name = "functionappkeys"
+
+  parameters = {
+    "functionApp" = "${data.azurerm_function_app.fn.name}"
+  }
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  deployment_mode       = "Incremental"
+
+  template_body = <<BODY
+  {
+      "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+      "contentVersion": "1.0.0.0",
+      "parameters": {
+          "functionApp": {"type": "string", "defaultValue": ""}
+      },
+      "variables": {
+          "functionAppId": "[resourceId('Microsoft.Web/sites', parameters('functionApp'))]"
+      },
+      "resources": [
+      ],
+      "outputs": {
+          "functionkey": {
+              "type": "string",
+              "value": "[listkeys(concat(variables('functionAppId'), '/host/default'), '2018-11-01').functionKeys.default]"                                                                                }
+      }
+  }
+  BODY
+}
+
+#output "func_keys" {
+#  sensitive = true
+#  value = "${lookup(azurerm_template_deployment.function_keys.outputs, "functionkey")}"
+#}
+
+resource "azurerm_eventgrid_event_subscription" "classifyBatch" {
+  name = "classifyBatchSubscription"
+  scope = data.azurerm_storage_account.storageAccountescrow.id
+
+  included_event_types = [
+    "Microsoft.Storage.BlobCreated"
+  ]
+
+  subject_filter {
+    subject_begins_with = "/blobServices/default/containers/transfer-"
+  }
+
+  webhook_endpoint {
+    # TODO: Add "&code="
+    url = "https://${data.azurerm_function_app.fn.default_hostname}/runtime/webhooks/eventgrid?functionName=AzureEventGridClassifyBatch?code=${lookup(azurerm_template_deployment.function_keys.outputs, "functionkey")}"
+  }
 }
