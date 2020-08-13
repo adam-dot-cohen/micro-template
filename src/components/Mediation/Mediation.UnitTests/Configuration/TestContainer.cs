194da9c6182509bde7f531926eed9ea58c2c70f1
Brandon Barry <brandon@quarterspot.com>
2020-08-13 11:49:36 -0500

    [ch4410] added some tests for the container registry

---
+++ src/components/Mediation/Mediation.UnitTests/Configuration/TestContainer.cs
@@ -0,0 +1,24 @@
+﻿using Lamar;
+using Laso.Mediation.Configuration.Lamar;
+using Microsoft.Extensions.Logging;
+
+namespace Laso.Mediation.UnitTests.Configuration
+{
+    public class TestContainer : Container
+    {
+        public TestContainer(Repository repository) : base(x =>
+        {
+            x.For(typeof(ILogger<>)).Use(typeof(InMemoryLogger<>));
+            x.For<Repository>().Use(repository);
+
+            x.Scan(scan =>
+            {
+                scan.AssemblyContainingType<Repository>();
+                scan.WithDefaultConventions();
+                scan.AddMediatorHandlers();
+            });
+
+            x.AddMediator().WithDefaultMediatorBehaviors();
+        }) { }
+    }
+}
