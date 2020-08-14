df87bd5dfc8e809617cfb7e3a965f2f83e829285
Jay McLain <jay_mclain@msn.com>
2020-08-14 00:34:27 -0500

    Enable service bus classify

--- src/products/Classification/AccountTransaction/AccountTransactionClassifier.Function/AzureServiceBusClassifyBatch.cs
+++ src/products/Classification/AccountTransaction/AccountTransactionClassifier.Function/AzureServiceBusClassifyBatch.cs
@@ -1,21 +1,63 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Laso.Scheduling.Core.IntegrationEvents.Publish.Scheduling;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Insights.AccountTransactionClassifier.Function
 {
     public class AzureServiceBusClassifyBatch
     {
+        private readonly IConfiguration _configuration;
         private readonly ILogger<AzureServiceBusClassifyBatch> _logger;
 
-        public AzureServiceBusClassifyBatch(ILogger<AzureServiceBusClassifyBatch> logger)
+        public AzureServiceBusClassifyBatch(IConfiguration configuration, ILogger<AzureServiceBusClassifyBatch> logger)
         {
+            _configuration = configuration;
             _logger = logger;
         }
 
         [FunctionName("AzureServiceBusClassifyBatch")]
-        public void Run([ServiceBusTrigger("scheduling", "AcctTxnClassifier.Function")] string message)
+        public Task Run(
+            [ServiceBusTrigger("scheduling", "AcctTxnClassifier.Function")] string message,
+            CancellationToken cancellationToken)
         {
             _logger.LogInformation($"AzureServiceBusClassifyBatch topic trigger function processing message: {message}");
+            var evt = JsonSerializer.Deserialize<ExperimentRunScheduledEvent>(message).Data;
+
+            var now = DateTime.UtcNow;
+            var process = new ClassifyBatchProcess();
+
+            return process.Run(
+                evt.PartnerId, 
+                Path.GetFileName(evt.Files.First().Uri),
+                $"Laso_QuarterSpot_R_AccountTransaction_{now:yyyyMMdd}_{now:yyyyMMddHHmmss}.csv",
+                _configuration,
+                _logger,
+                cancellationToken);
         }
+    }
+
+    public abstract class CloudEventEnvelope<TData>
+    {
+        public string Id { get; set; } = null!;
+        public Uri Source { get; set; } = null!;
+        public string SpecVersion { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public string DataSchema { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public DateTime Time { get; set; }
+        public TData Data { get; set; } = default!;
+    }
+
+    public class ExperimentRunScheduledEvent : CloudEventEnvelope<ExperimentRunScheduledEventV1>
+    {
     }
 }
--- src/products/Classification/AccountTransaction/AccountTransactionClassifier.Function/ClassifyBatchProcess.cs
+++ src/products/Classification/AccountTransaction/AccountTransactionClassifier.Function/ClassifyBatchProcess.cs
@@ -3,10 +3,13 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Storage.Blobs;
 using Insights.AccountTransactionClassifier.Function.Azure;
 using Insights.AccountTransactionClassifier.Function.Classifier;
 using Insights.AccountTransactionClassifier.Function.Normalizer;
 using Laso.Catalog.Domain.FileSchema.Input;
+using Laso.IO.Structured;
 //using Laso.IO.Structured;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -15,92 +18,92 @@ namespace Insights.AccountTransactionClassifier.Function
 {
     public class ClassifyBatchProcess
     {
-        //public async Task Run(
-        //    string partnerId,
-        //    string inputFilename,
-        //    string outputFilename,
-        //    IConfiguration configuration,
-        //    ILogger logger,
-        //    CancellationToken cancellationToken)
-        //{
-        //    logger.LogDebug("Opening input blob file.");
-        //    var storage = GetStorage(configuration);
-        //    var inputStream = await BlobOpenRead(storage, partnerId, inputFilename, cancellationToken);
+        public async Task Run(
+            string partnerId,
+            string inputFilename,
+            string outputFilename,
+            IConfiguration configuration,
+            ILogger logger,
+            CancellationToken cancellationToken)
+        {
+            logger.LogDebug("Opening input blob file.");
+            var storage = GetStorage(configuration);
+            var inputStream = await BlobOpenRead(storage, partnerId, inputFilename, cancellationToken);
 
-        //    logger.LogDebug("Creating file reader.");
-        //    using var reader = new DelimitedFileReader();
-        //    reader.Open(inputStream);
+            logger.LogDebug("Creating file reader.");
+            using var reader = new DelimitedFileReader();
+            reader.Open(inputStream);
 
-        //    logger.LogDebug("Reading transactions.");
-        //    var transactions = reader
-        //        .ReadRecords<AccountTransaction_v0_3>()
-        //        .ToList();
+            logger.LogDebug("Reading transactions.");
+            var transactions = reader
+                .ReadRecords<AccountTransaction_v0_3>()
+                .ToList();
 
-        //    logger.LogDebug("Classifying transactions.");
-        //    var normalizer = new AccountTransactionNormalizer();
-        //    var creditsMachineLearningService = new AzureMachineLearningService(new RetryPolicy())
-        //    {
-        //        BaseUrl = configuration["AzureCreditsBankTransactionClassifierApiEndpoint"],
-        //        ApiKey = configuration["AzureCreditsBankTransactionClassifierApiKey"]
-        //    };
-        //    var debitsMachineLearningService = new AzureMachineLearningService(new RetryPolicy())
-        //    {
-        //        BaseUrl = configuration["AzureDebitsBankTransactionClassifierApiEndpoint"],
-        //        ApiKey = configuration["AzureDebitsBankTransactionClassifierApiKey"]
-        //    };
-        //    var classifier = new AzureBankAccountTransactionClassifier(normalizer, creditsMachineLearningService, debitsMachineLearningService);
-        //    var classes = await classifier.Classify(transactions, cancellationToken);
+            logger.LogDebug("Classifying transactions.");
+            var normalizer = new AccountTransactionNormalizer();
+            var creditsMachineLearningService = new AzureMachineLearningService(new RetryPolicy())
+            {
+                BaseUrl = configuration["AzureCreditsBankTransactionClassifierApiEndpoint"],
+                ApiKey = configuration["AzureCreditsBankTransactionClassifierApiKey"]
+            };
+            var debitsMachineLearningService = new AzureMachineLearningService(new RetryPolicy())
+            {
+                BaseUrl = configuration["AzureDebitsBankTransactionClassifierApiEndpoint"],
+                ApiKey = configuration["AzureDebitsBankTransactionClassifierApiKey"]
+            };
+            var classifier = new AzureBankAccountTransactionClassifier(normalizer, creditsMachineLearningService, debitsMachineLearningService);
+            var classes = await classifier.Classify(transactions, cancellationToken);
 
-        //    logger.LogDebug("Writing classes.");
-        //    using var writer = new DelimitedFileWriter();
-        //    await using var outputStream = new MemoryStream();
-        //    writer.Open(outputStream);
-        //    writer.WriteRecords(classes);
-        //    outputStream.Seek(0, SeekOrigin.Begin);
+            logger.LogDebug("Writing classes.");
+            using var writer = new DelimitedFileWriter();
+            await using var outputStream = new MemoryStream();
+            writer.Open(outputStream);
+            writer.WriteRecords(classes);
+            outputStream.Seek(0, SeekOrigin.Begin);
 
-        //    logger.LogDebug("Saving output blob file.");
-        //    await BlobWrite(storage, partnerId, outputFilename, outputStream, cancellationToken);
-        //}
+            logger.LogDebug("Saving output blob file.");
+            await BlobWrite(storage, partnerId, outputFilename, outputStream, cancellationToken);
+        }
 
-        //private static BlobServiceClient GetStorage(IConfiguration configuration)
-        //{
-        //    var storageUrl = new Uri(configuration["Services:Provisioning:PartnerEscrowStorage:ServiceUrl"]);
-        //    return new BlobServiceClient(storageUrl, new DefaultAzureCredential());
-        //}
+        private static BlobServiceClient GetStorage(IConfiguration configuration)
+        {
+            var storageUrl = new Uri(configuration["Services:Provisioning:PartnerEscrowStorage:ServiceUrl"]);
+            return new BlobServiceClient(storageUrl, new DefaultAzureCredential());
+        }
 
-        //private static async Task<Stream> BlobOpenRead(
-        //    BlobServiceClient storage, 
-        //    string partnerId,
-        //    string filename,
-        //    CancellationToken cancellationToken)
-        //{
-        //    var fileSystemName = $"transfer-{partnerId}";
-        //    var container = storage.GetBlobContainerClient(fileSystemName);
-        //    var inputFilename = $"incoming/{filename}";
-        //    var blob = container.GetBlobClient(inputFilename);
+        private static async Task<Stream> BlobOpenRead(
+            BlobServiceClient storage,
+            string partnerId,
+            string filename,
+            CancellationToken cancellationToken)
+        {
+            var fileSystemName = $"transfer-{partnerId}";
+            var container = storage.GetBlobContainerClient(fileSystemName);
+            var inputFilename = $"incoming/{filename}";
+            var blob = container.GetBlobClient(inputFilename);
 
-        //    // TODO: Needs - "Storage Blob Data Reader" role in Azure IAM
-        //    var response = await blob.DownloadAsync(cancellationToken);
+            // TODO: Needs - "Storage Blob Data Reader" role in Azure IAM
+            var response = await blob.DownloadAsync(cancellationToken);
 
-        //    return response.Value.Content;
-        //}
+            return response.Value.Content;
+        }
 
-        //private static async Task<string> BlobWrite(
-        //    BlobServiceClient storage, 
-        //    string partnerId,
-        //    string filename,
-        //    Stream outputStream,
-        //    CancellationToken cancellationToken)
-        //{
-        //    var fileSystemName = $"transfer-{partnerId}";
-        //    var container = storage.GetBlobContainerClient(fileSystemName);
-        //    var outputFilename = $"outgoing/{filename}";
-        //    var blob = container.GetBlobClient(outputFilename);
+        private static async Task<string> BlobWrite(
+            BlobServiceClient storage,
+            string partnerId,
+            string filename,
+            Stream outputStream,
+            CancellationToken cancellationToken)
+        {
+            var fileSystemName = $"transfer-{partnerId}";
+            var container = storage.GetBlobContainerClient(fileSystemName);
+            var outputFilename = $"outgoing/{filename}";
+            var blob = container.GetBlobClient(outputFilename);
 
-        //    // TODO: Needs - "Storage Blob Data Contributor" role in Azure IAM
-        //    var response = await blob.UploadAsync(outputStream, cancellationToken);
+            // TODO: Needs - "Storage Blob Data Contributor" role in Azure IAM
+            var response = await blob.UploadAsync(outputStream, cancellationToken);
 
-        //    return response.Value.ETag.ToString();
-        //}
+            return response.Value.ETag.ToString();
+        }
     }
 }
