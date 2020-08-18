f2bee7b0c4b99c07285eaf49af5bc346b9d8165e
Jay McLain <jay_mclain@msn.com>
2020-08-18 11:16:52 -0500

    Fix path to terraform for Idenitty [ch4452]

--- deploy/azure/terraform/applications/Insights/identity-service/apply-dev.ps1
+++ deploy/azure/terraform/applications/Insights/identity-service/apply-dev.ps1
@@ -1,5 +1,1 @@
-terraform apply -auto-approve -var-file ../../../environments/dev/terraform.tfvars -var-file environments/dev.tfvars -var="buildNumber=1.0.0.3747-pre"
-
-
-
- 
+..\..\..\bin\terraform apply -auto-approve -var-file ../../../environments/dev/terraform.tfvars -var-file environments/dev.tfvars -var="buildNumber=1.0.0.3747-pre"
