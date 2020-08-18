b32ee0cd6ccd51883272685ac7af86fcc37d04f6
Jay McLain <jay_mclain@msn.com>
2020-08-18 13:54:45 -0500

    Add staging vars for catalog and subscription [ch4452]

--- deploy/azure/terraform/applications/Insights/subscription-service/destroy-dev.ps1
+++ deploy/azure/terraform/applications/Insights/subscription-service/destroy-dev.ps1
@@ -1,1 +1,1 @@
-terraform destroy  -var="buildNumber=<Your Build Number>" -var-file ./secrets/secrets.tfvars -var-file ../../environments/dev/terraform.tfvars
+..\..\..\bin\terraform destroy  -var="buildNumber=<Your Build Number>" -var-file ./secrets/secrets.tfvars -var-file ../../environments/dev/terraform.tfvars
