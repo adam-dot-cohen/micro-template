3ce16e6cf8e197deb1fda93fbf32276ddb8023f2
Jay McLain <jay_mclain@msn.com>
2020-08-27 10:43:35 -0500

    Update tag formatting and add for app services. [ch4512]

--- deploy/azure/terraform/modules/common/appservice/appservice.tf
+++ deploy/azure/terraform/modules/common/appservice/appservice.tf
@@ -9,7 +9,7 @@ variable "application_environment"{
 }
 
 variable "service_settings"{  
-    description = "Container version, docer repository name, and capacity for VMs,etc"
+    description = "Container version, Docker repository name, and capacity for VMs,etc"
     type = object({ tshirt = string, 
     buildNumber = string, 
     instanceName = string, 
@@ -26,9 +26,6 @@ variable "app_settings"{
 }
 
 
-
-
-
 ##############
 # LOOKUP
 ##############
@@ -42,7 +39,7 @@ module "resourceNames" {
 }
 
 
-locals{
+locals {
   tier          = "Basic"
   size          = "B1"
   kind          = "Linux"
@@ -54,26 +51,23 @@ data "azurerm_resource_group" "rg" {
   name = module.resourceNames.resourceGroup
 }
 
-data "azurerm_subscription" "current" {}
+data "azurerm_subscription" "current" {
+}
 
 data "azurerm_container_registry" "acr" {
-  name                     = module.resourceNames.containerRegistry
-  resource_group_name 		= data.azurerm_resource_group.rg.name
+  name                = module.resourceNames.containerRegistry
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 data "azurerm_application_insights" "ai" {
-  name                     = module.resourceNames.applicationInsights
-  resource_group_name 		= data.azurerm_resource_group.rg.name
+  name                = module.resourceNames.applicationInsights
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
-
-
 
 data "azurerm_key_vault" "kv" {
-  name                     = module.resourceNames.keyVault
-  resource_group_name 		= data.azurerm_resource_group.rg.name
+  name                = module.resourceNames.keyVault
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
-
-
 
 locals {
   app_settings = {
@@ -81,14 +75,14 @@ locals {
     #We don't use this becuase it throws off the client side.  
     # we need to revisit if we want to use appsettings.{env}.config overrides though.
 
-    DOCKER_REGISTRY_SERVER_URL                = "https://${data.azurerm_container_registry.acr.login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME           = "${data.azurerm_container_registry.acr.admin_username}"
-    DOCKER_REGISTRY_SERVER_PASSWORD           = "${data.azurerm_container_registry.acr.admin_password}"
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE       = false
-    DOCKER_ENABLE_CI						  = true
-    ASPNETCORE_FORWARDEDHEADERS_ENABLED = true
-    Laso__Logging__Common__Environment = module.resourceNames.environments[var.application_environment.environment].name
-    ApplicationInsights__InstrumentationKey       = data.azurerm_application_insights.ai.instrumentation_key
+    DOCKER_REGISTRY_SERVER_URL              = "https://${data.azurerm_container_registry.acr.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME         = "${data.azurerm_container_registry.acr.admin_username}"
+    DOCKER_REGISTRY_SERVER_PASSWORD         = "${data.azurerm_container_registry.acr.admin_password}"
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE     = false
+    DOCKER_ENABLE_CI						            = true
+    ASPNETCORE_FORWARDEDHEADERS_ENABLED     = true
+    Laso__Logging__Common__Environment      = module.resourceNames.environments[var.application_environment.environment].name
+    ApplicationInsights__InstrumentationKey = data.azurerm_application_insights.ai.instrumentation_key
   }
 }
 
@@ -97,13 +91,21 @@ resource "azurerm_app_service_plan" "adminAppServicePlan" {
   name                = "${module.resourceNames.applicationServicePlan}-${var.service_settings.instanceName}"
   location            = module.resourceNames.regions[var.application_environment.region].cloudRegion
   resource_group_name = data.azurerm_resource_group.rg.name
-  kind = local.kind
-  reserved = true
+  kind                = local.kind
+  reserved            = true
+
   sku {
-    tier = local.tier
-    size = local.size
-    capacity=var.service_settings.capacity
-  } 
+    tier      = local.tier
+    size      = local.size
+    capacity  = var.service_settings.capacity
+  }
+
+  tags = {
+    Environment = module.resourceNames.environments[var.application_environment.environment].name
+    Role = title(var.application_environment.role)
+	  Tenant = title(var.application_environment.tenant)
+	  Region = module.resourceNames.regions[var.application_environment.region].locationName
+  }
 }
 
 resource "azurerm_app_service" "adminAppService" {
@@ -111,17 +113,23 @@ resource "azurerm_app_service" "adminAppService" {
   location            = module.resourceNames.regions[var.application_environment.region].cloudRegion
   resource_group_name = data.azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.adminAppServicePlan.id
-  app_settings = merge(var.app_settings,local.app_settings)
-
-
+  app_settings        = merge(var.app_settings,local.app_settings)
 
   # Configure Docker Image to load on start
   site_config {
     linux_fx_version = "DOCKER|${data.azurerm_container_registry.acr.name}.azurecr.io/${var.service_settings.dockerRepo}:${var.service_settings.buildNumber}"
     always_on        = local.alwaysOn
   }
+
   identity {
     type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = module.resourceNames.environments[var.application_environment.environment].name
+    Role = title(var.application_environment.role)
+	  Tenant = title(var.application_environment.tenant)
+	  Region = module.resourceNames.regions[var.application_environment.region].locationName
   }
 }
 
