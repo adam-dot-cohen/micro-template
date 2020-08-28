0890b07bdf2dcaecf93c6e1a06fdebf9615149ac
mpquarterspot <m.pantazopoulos@quarterspot.com>
2020-08-28 16:00:45 -0500

    fixes [ch4465]

--- src/tests/Insights.UITests/Tests/InsightsManagerPortal/Partners/CreateAndProvisionPartnersTests.cs
+++ src/tests/Insights.UITests/Tests/InsightsManagerPortal/Partners/CreateAndProvisionPartnersTests.cs
@@ -48,22 +48,19 @@ namespace Insights.UITests.Tests.InsightsManagerPortal.Partners
         }
 
         [Test]
-        [Order(1)]
         public void ValidateSnackbarPartnerSaved()
         {
             Assert.True(_partnerSaved, "A snackbar should be displayed when a partner has been saved");
         }
 
         [Test]
-        [Order(2)]
         public void ValidateSnackbarPartnerProvisioned()
         {
             Assert.True(_partnerProvisioned, "A snackbar should be displayed when a partner has been provisioned");
         }
 
-
         [Test]
-        [Order(3)]
+        [Order(1)]
         public void ValidatePartnerIsListedOnPartnerPage()
         {
             var actualPartner =
@@ -75,14 +72,13 @@ namespace Insights.UITests.Tests.InsightsManagerPortal.Partners
         }
 
         [Test]
-        [Order(4)]
+        [Order(2)]
         public void ValidatePartnerContactInfoOnPartnersPage()
         {
             if (!_partnerListed)
                 Assert.Ignore("Partner not listed: " + expectedPartner.Name + " , test case cannot be executed");
             var actualPartner =
                 Go.To<PartnersPage>().FindPartner(expectedPartner);
-
             new AssertObjectComparer<Partner>()
                 .Compare(actualPartner, expectedPartner, new[] {nameof(Partner.ContactName)});
         }
@@ -102,11 +98,11 @@ namespace Insights.UITests.Tests.InsightsManagerPortal.Partners
 
 
         [Test]
+        [Order(2)]
         public void PartnerDetailsPageValidation()
         {
-            if (!_partnerSaved)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed");
+            if (!_partnerListed)
+                Assert.Ignore("Partner not listed: " + expectedPartner.Name + " , test case cannot be executed");
 
             var actualPartnerOnDetailsPage =
                 Go.To<PartnersPage>()
@@ -116,146 +112,134 @@ namespace Insights.UITests.Tests.InsightsManagerPortal.Partners
                 .Compare(actualPartnerOnDetailsPage, expectedPartner);
         }
 
-        private List<Configuration> _partnerConfiguration;
 
         [Test]
-        [Order(5)]
+        [Order(2)]
         public void PartnerConfigurationValidationSFTPConfigurationUserName()
         {
-            _partnerConfiguration =
-                Go.To<PartnersPage>()
-                    .SelectPartnerCard<PartnersDetailsPage>(expectedPartner)
-                    .SelectViewPartnerConfiguration()
-                    .ConfigurationOnPartnerConfigurationPage();
-
-
+            var configurationsList =
+                RetrievePartnerConfiguration();
             var configItem =
-                _partnerConfiguration.Single(x =>
-                    x.Name.Contains("User Name") && x.Category.Equals("SFTPUsername"));
+                configurationsList
+                    .Single(x =>
+                        x.Name.Contains("User Name") && x.Category.Equals("SFTPUsername"));
             Assert.True(configItem.Value.Contains(expectedPartner.Name.Replace(" ", "").ToLowerInvariant()),
                 "Validating the Vale for User Name configuration Name includes the partner name");
         }
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationSFTPConfigurationPassword()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
-            var configItem =
-                _partnerConfiguration.Single(x => x.Name.Equals("sFTP Password") && x.Category.Equals("SFTPPassword"));
+            var configurationsList =
+                RetrievePartnerConfiguration();
+            var configItem = configurationsList
+                .Single(x => x.Name.Equals("sFTP Password") && x.Category.Equals("SFTPPassword"));
             Assert.False(string.IsNullOrEmpty(configItem.Value),
                 "The password configuration item should have a value ");
         }
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationLasoPGPPublicKey()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
+            var configurationsList =
+                RetrievePartnerConfiguration();
             var configItem =
-                _partnerConfiguration.Single(x =>
+                configurationsList.Single(x =>
                     x.Name.Equals("Laso Public Key") && x.Category.Equals("LasoPGPPublicKey"));
             Assert.True(configItem.Value.Contains("BEGIN PGP PUBLIC KEY BLOCK"),
                 "Validating Laso Public Key was set to a value starting with -BEGIN PGP PUBLIC KEY BLOCK ");
         }
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationLasoPGPPrivateKey()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
+            var configurationsList =
+                RetrievePartnerConfiguration();
+
             var configItem =
-                _partnerConfiguration.Single(x =>
+                configurationsList.Single(x =>
                     x.Name.Equals("Laso Private Key") && x.Category.Equals("LasoPGPPrivateKey"));
             Assert.True(configItem.Value.Contains("BEGIN PGP PRIVATE KEY BLOCK"),
                 "Validating the LasoPGPPrivateKey was set to a value starting with -BEGIN PGP PRIVATE KEY BLOCK ");
         }
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationLasoPGPPassphrase()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
+            var configurationsList =
+                RetrievePartnerConfiguration();
             var configItem =
-                _partnerConfiguration.Single(x =>
+                configurationsList.Single(x =>
                     x.Name.Equals("Laso PGP Pass Phrase") && x.Category.Equals("LasoPGPPassphrase"));
             Assert.False(string.IsNullOrEmpty(configItem.Value),
                 "The LasoPGPPassphrase configuration item should have a value ");
         }
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationPublishedFileSystem()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
+            var configurationsList =
+                RetrievePartnerConfiguration();
             var configItem =
-                _partnerConfiguration.Single(x =>
+                configurationsList.Single(x =>
                     x.Name.Contains("published Directory") && x.Category.Equals("PublishedFileSystemDirectory"));
             Assert.False(string.IsNullOrEmpty(configItem.Value),
                 "The published file system configuration item should have a value ");
         }
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationRejectedFileSystemDirectory()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
+            var configurationsList =
+                RetrievePartnerConfiguration();
             var configItem =
-                _partnerConfiguration.Single(x =>
+                configurationsList.Single(x =>
                     x.Name.Contains("rejected Directory") && x.Category.Equals("RejectedFileSystemDirectory"));
             Assert.False(string.IsNullOrEmpty(configItem.Value),
                 "The rejected directory configuration item should have a value ");
         }
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationCuratedFileSystemDirectory()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
+            var configurationsList =
+                RetrievePartnerConfiguration();
             var configItem =
-                _partnerConfiguration.Single(x =>
+                configurationsList.Single(x =>
                     x.Name.Contains("curated Directory") && x.Category.Equals("CuratedFileSystemDirectory"));
             Assert.False(string.IsNullOrEmpty(configItem.Value),
                 "The curated Directory configuration item should have a value ");
         }
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationRawFileSystemDirectory()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
+            var configurationsList =
+                RetrievePartnerConfiguration();
+
             var configItem =
-                _partnerConfiguration.Single(x =>
+                configurationsList.Single(x =>
                     x.Name.Contains("raw Directory") && x.Category.Equals("RawFileSystemDirectory"));
             Assert.False(string.IsNullOrEmpty(configItem.Value),
                 "The raw Directory configuration item should have a value ");
         }
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationColdStorage()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
+            var configurationsList =
+                RetrievePartnerConfiguration();
             var configItem =
-                _partnerConfiguration.Single(x =>
+                configurationsList.Single(x =>
                     x.Name.Contains("Cold Storage Container") && x.Category.Equals("ColdStorage"));
             Assert.False(string.IsNullOrEmpty(configItem.Value),
                 "The Cold Storage Container configuration item should have a value ");
@@ -263,17 +247,31 @@ namespace Insights.UITests.Tests.InsightsManagerPortal.Partners
 
 
         [Test]
-        [Order(6)]
+        [Order(2)]
         public void PartnerConfigurationValidationEscrowStorage()
         {
-            if (_partnerConfiguration == null || _partnerConfiguration.Count == 0)
-                Assert.Inconclusive(AtataContext.Current.TestName +
-                                    "is inconclusive as test case CreateAndProvisionPartnerAllRequiredFields did not succeed. Partner was not provisioned configuration cannot be validated");
+            var configurationsList =
+                RetrievePartnerConfiguration();
             var configItem =
-                _partnerConfiguration.Single(x =>
+                configurationsList.Single(x =>
                     x.Name.Contains("Escrow Container") && x.Category.Equals("EscrowStorage"));
             Assert.False(string.IsNullOrEmpty(configItem.Value),
                 "The Escrow Storage Container configuration item should have a value ");
+        }
+
+        private List<Configuration> RetrievePartnerConfiguration()
+        {
+            if (!_partnerListed)
+                Assert.Ignore("Partner not listed: " + expectedPartner.Name + " , test case cannot be executed");
+
+            var configurationsList =
+                Go.To<PartnersPage>()
+                    .SelectPartnerCard<PartnersDetailsPage>(expectedPartner)
+                    .SelectViewPartnerConfiguration()
+                    .ConfigurationOnPartnerConfigurationPage();
+            Assert.True(configurationsList != null && configurationsList.Count > 1,
+                "The configurations table should be populated");
+            return configurationsList;
         }
 
         [TestCaseSource(nameof(PartnerTestData))]
