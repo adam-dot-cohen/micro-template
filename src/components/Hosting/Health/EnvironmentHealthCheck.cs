a4b72bd04369799c7e1a719378076c6cb1e19f96
Jay McLain <jay_mclain@msn.com>
2020-08-31 14:07:03 -0500

    Add EnvrionmentHealthCheck to all services

---
+++ src/components/Hosting/Health/EnvironmentHealthCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Laso.Hosting.Health
+{
+    public class EnvironmentHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var response = new Dictionary<string, object?>
+            {
+                ["MachineName"] = Environment.MachineName,
+                ["Version"] = Assembly.GetEntryAssembly()?.GetName().Version.ToString(4),
+                ["ServerTime"] = DateTime.UtcNow.ToString("O")
+            };
+
+            return Task.FromResult(HealthCheckResult.Healthy(data: response));
+        }
+    }
+}
