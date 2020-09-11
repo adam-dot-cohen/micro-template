3bbffde0d3c9b4df13a3750bce928e7bc00bd373
Matt Scully <matt.scully@quarterspot.com>
2020-09-11 10:41:32 -0500

    Update all *-apply.ps1 files to reference plan artifacts [ch4550]

--- deploy/azure/terraform/applications/Insights/identity-service/dev-apply.ps1
+++ deploy/azure/terraform/applications/Insights/identity-service/dev-apply.ps1
@@ -1,1 +1,1 @@
-..\..\..\bin\terraform apply -auto-approve -var-file ../../../environments/dev/terraform.tfvars -var-file environments/dev.tfvars -var="buildNumber=1.0.0.5679-pre"
+..\..\..\bin\terraform apply -auto-approve artifact_dev
